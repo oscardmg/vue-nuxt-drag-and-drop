5f218128b1e8f97f19dee1a54ce5ed50c7b48550
Oscar Montoya <oscardmg@gmail.com>
2019-08-16 14:16:06 -0500

    primer commit

---
+++ components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    :id="id"
+    class="card"
+    :draggable="draggable"
+    @dragstart="dragStart"
+    @dragover.stop
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: [
+    'id', 'draggable'
+  ],
+  methods: {
+    dragStart (e) {
+      const target = e.target;
+      console.log(e);
+      console.log(target.id);
+      e.dataTransfer.setData('card_id', target.id);
+      setTimeout(() => {
+        target.style.display = 'none';
+      }, 0);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,47 @@
 <template>
-  <v-content>
-    <v-container grid-list-sm>
-      <v-layout
-        row
-        wrap
-      >
-        <v-flex
-          xs6
-        >
-          <v-card>
-            <p>Col1</p>
-          </v-card>
-        </v-flex>
-        <v-flex
-          xs6
-        >
-          <v-card>
-            <p>Col2</p>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-content>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <main class="flexbox">
+      <board id="board-1">
+        <card v-for="card in cards" :key="card.id" :id="'card' + card.id" draggable="true">
+          <p>Card {{ card.id }}</p>
+        </card>
+      </board>
+
+      <board id="board-2">
+        <card v-for="card in cards1" :key="card.id" :id="'card' + card.id" draggable="true">
+          <p>Card {{ card.id }}</p>
+        </card>
+      </board>
+    </main>
+    <board />
+  </v-layout>
 </template>
 
 <script>
+import Board from '@/components/Board';
+import Card from '@/components/Card';
 
 export default {
-
+  components: {
+    Board, Card
+  },
+  data () {
+    return {
+      cards: [
+        { id: 'one' },
+        { id: 'two' },
+        { id: 'three' }
+      ],
+      cards1: [
+        { id: 'four' },
+        { id: 'five' },
+        { id: 'six' }
+      ]
+    };
+  }
 };
 </script>
